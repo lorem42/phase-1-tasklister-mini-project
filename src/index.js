9c31e626d83ff6ed889b842b8b17e622c0e7c2e9
Sondos Ahmed Ibrahim <sunnysondos42@gmail.com>
2022-10-04 22:50:42 +0300

    assignment complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const btn = document.querySelector('[type="submit"]')
+  btn.addEventListener("click", function (btn) {
+    btn.preventDefault();
+
+    const input = document.querySelector('[type="text"]');
+    const li = document.createElement("li");
+    li.textContent = input.value;
+    const tasks = document.getElementById("tasks");
+    tasks.appendChild(li);
+    const del = document.createElement("button");
+    del.innerHTML = "x";
+    li.appendChild(del);
+    del.addEventListener("click", deleteLi)
+    input.value = "";
+
+  });
+
+  function deleteLi(e) {
+    e.target.parentNode.remove();
+  }
+
 });
+
+
+
